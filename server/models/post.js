a2afe7d4861c1e711740fde14c8ce294945a7696
tamirameba <tamirameba50014@gmail.com>
2024-07-28 01:01:48 +0300

    create my database

---
+++ server/models/post.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+ const schema=mongoose.schema;
+ const postSchema=new schema({
+    title:{
+        type:String,
+        required:true
+
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    updatedAt: {
+        type:Date,
+        default:Date.now
+    }
+    
+ });
+ module.exports=mongoose.model('post',postSchema);
