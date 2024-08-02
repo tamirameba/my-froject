a79d773cafd71ce58a9502bcf4f11953070eb3cb
tamirameba <tamirameba50014@gmail.com>
2024-08-02 19:55:42 +0300

    Add encryption

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
-const UserModel = require('../models/user')
+const UserModel = require('../models/user');
+const { PostModel } = require('../models/post');
 
-router.get('/', (req, res) => {
-  const locals = {
-    title: "blog",
-    description: "blog application post and comments on articles",
-  };
-  res.render('index.ejs');
+
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt()
+  const hashed = await bcrypt.hash(password, salt)
+  return hashed
+}
+
+router.get('/', async (req, res) => {
+
+
+
+
+  console.log(req.session?.user)
+  res.render('index.ejs', {
+    name: req.session?.user?.fullName ?? '',
+    posts: req.session?.user ? await PostModel.find() : [],
+    isLoggedIn: !!req.session?.user
+  });
 });
 
 router.get('/users', (req, res) => {
@@ -31,21 +45,33 @@ router.get('/login', (req, res) => {
 
 router.post('/api/register', async (req, res) => {
   console.log(req.body);
-  const user = new UserModel(req.body);
+  const hashedPassword = await hashPassword(req.body.password);
+  console.log({ hashedPassword })
+  const user = new UserModel({ ...req.body, password: hashedPassword });
   await user.save()
 
-  res.json(user)
+  res.redirect('/login')
 });
 
 router.post('/api/login', async (req, res) => {
   console.log(req.body);
-  const user = await UserModel.findOne(req.body);
+  const user = await UserModel.findOne({ email: req.body.email });
   if (!user) {
-    const message = encodeURIComponent("Invalid user or password")
+    const message = encodeURIComponent("Invalid email")
     return res.redirect('/login?message=' + message)
   }
 
-  res.json(user)
+  console.log(user)
+  console.log(req.body.password, user.password)
+  const isValidPassword = bcrypt.compareSync(req.body.password, user.password)
+  if (!isValidPassword) {
+    const message = encodeURIComponent("Invalid email or password")
+    return res.redirect('/login?message=' + message)
+  }
+
+  req.session.user = user;
+
+  res.redirect('/')
 });
 
 module.exports = router;
